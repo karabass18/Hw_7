ef56992e2479f2b0d512533870876e352cbe16b7
Aleksei Dementev <karabass18@gmail.com>
2022-11-29 02:19:08 +0500

    added csvSource

--- src/test/java/com/karabas18/ParamTests.java
+++ src/test/java/com/karabas18/ParamTests.java
@@ -1,24 +1,52 @@
 package com.karabas18;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.awt.*;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ParamTests {
     @BeforeAll
-        //Configuration.holdBrowserOpen = true;
+    //Configuration.holdBrowserOpen = true;
     static void setStart() {
+        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
-        open("https://sport-marafon.ru/");
+       // open("https://sport-marafon.ru/");
+    }
+    @Test
+    void sportMarafonTestConsult(){
+        open("https://sport-marafon.ru/consultation/");
+        $(".form__item").scrollIntoView(true);
+        $(".custom-control-label").click();
+        $(".form__select > .custom-select-container > .custom-select-opener").click();
+        $("[data-value=\"Telegram\"]").click();
+
+        Condition v =visible;
+        $("#telegram-name").shouldBe(v);
+                //$(withText("Туристическое снаряжение")).doubleClick();
+    }
+
+
+    @Disabled
+    @ValueSource(strings = {"Каталог", "Одежда", "Обувь", "Туризм", "Бег", "Альпинизм",
+            "Путешествия", "Горные лыжи", "Сноуборд"})
+    @ParameterizedTest(name = "Проверка соответствия названия раздела {0} " +
+            "в верхнем меню сайта надписи в хлебных крошках")
+    void sportMarafonTestMenu(String section) {
+        $(".shop-menu__wrap").$(byText(section)).click();
+        $(".breadcrumbs__wrap").shouldHave(text(section));
     }
 
     @CsvSource(value = {
@@ -27,38 +55,17 @@ public class ParamTests {
             "13447| MSR| Habitude 4",
             "130259| Robens| Starlight 2 Green"
     },
-    delimiter = '|')
-
+            delimiter = '|')
+    @Disabled
     @ParameterizedTest(name = "Проверка наличия в выдаче поиска по коду {0} товара" +
-                        " названий бренда {1} и модели {2}")
-    void sportMarafonTest(String serchQuery, String expectedBrand, String expectedName) {
+            " названий бренда {1} и модели {2}")
+    void sportMarafonTestSearch(String searchQuery, String expectedBrand, String expectedName) {
         $(".header__search").click();
-        $("#head-search-input").setValue(serchQuery).pressEnter();
+        $("#head-search-input").setValue(searchQuery).pressEnter();
         $(".product-list__brand").shouldHave(text(expectedBrand));
         $(".product-list__name").shouldHave(text(expectedName));
 
-       /* $(".header__search").click();
-        $("#head-search-input").setValue("91163LAAFR").pressEnter();
-        $(".product-list__name").shouldHave(text("Палатка Ferrino Pilier 3 Fr Orange"));
-        $(".header__search").click();
-        $("#head-search-input").setValue("13447").pressEnter();
-        $(".product-list__brand").shouldHave(text("MSR"));
-        $(".product-list__name").shouldHave(text("Палатка MSR Habitude 4"));
-        /* open("https://detmir.ru");
-        $(".e_6").$(byText("Поиск")).click();
-        $(".s_9").setValue("Lego").pressEnter();
-        switchTo().frame("fl-403933");
-
-        $(".close").click();
-        //switchTo().defaultContent();
-        $(".hQ hU e_8").click();
-        //$(".e_6").$(byText("Поиск")).click();
-        // switchTo.Frame("fl-403933");
-        //$(".hZ").click();
-        //$(withText("Нажмите «Разрешить» в углу")).doubleClick();
-        //$(".data-mutation").click()
-        //$(".xH xL xK").scrollTo();
-        // $(".widget js-widget").
-        //$(byText("Поиск")).   .$(byText("Поиск"))*/
     }
+
+
 }
