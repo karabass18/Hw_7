da8b659107a17ede93b62cb761769a5565092bf1
Aleksei Dementev <karabass18@gmail.com>
2022-11-29 22:03:07 +0500

    added methodSource

--- src/test/java/com/karabas18/ParamTests.java
+++ src/test/java/com/karabas18/ParamTests.java
@@ -3,52 +3,63 @@ package com.karabas18;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.awt.*;
+import java.util.stream.Stream;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class ParamTests {
     @BeforeAll
-    //Configuration.holdBrowserOpen = true;
     static void setStart() {
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
-       // open("https://sport-marafon.ru/");
     }
-    @Test
-    void sportMarafonTestConsult(){
+
+    //
+    //    @MethodSource
+    //
+    static Stream<Arguments> sportMarafonTestConsult() {
+        return Stream.of(
+                Arguments.of("Телефон", hidden),
+                Arguments.of("WhatsApp", hidden),
+                Arguments.of("Telegram", visible)
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Проверка видимости поля nickname если выбран способ связи {0}")
+    void sportMarafonTestConsult(String contact, Condition visibleOrHidden) {
         open("https://sport-marafon.ru/consultation/");
-        $(".form__item").scrollIntoView(true);
+        $(byText("Какой отдел вас интересует?")).scrollIntoView(false);
         $(".custom-control-label").click();
         $(".form__select > .custom-select-container > .custom-select-opener").click();
-        $("[data-value=\"Telegram\"]").click();
-
-        Condition v =visible;
-        $("#telegram-name").shouldBe(v);
-                //$(withText("Туристическое снаряжение")).doubleClick();
+        $("[data-value=\"" + contact + "\"]").click();
+        $("#telegram-name").shouldBe(visibleOrHidden);
     }
 
-
-    @Disabled
+    //
+    //@ValueSource
+    //
     @ValueSource(strings = {"Каталог", "Одежда", "Обувь", "Туризм", "Бег", "Альпинизм",
             "Путешествия", "Горные лыжи", "Сноуборд"})
     @ParameterizedTest(name = "Проверка соответствия названия раздела {0} " +
             "в верхнем меню сайта надписи в хлебных крошках")
     void sportMarafonTestMenu(String section) {
+        open("https://sport-marafon.ru/");
         $(".shop-menu__wrap").$(byText(section)).click();
         $(".breadcrumbs__wrap").shouldHave(text(section));
     }
 
+    //
+    // @CsvSource
+    //
     @CsvSource(value = {
             "91163LAAFR| Ferrino| Pilier 3 Fr Orange",
             "91188LIIFR| Ferrino| Grit 2 Fr Light Grey",
@@ -56,16 +67,15 @@ public class ParamTests {
             "130259| Robens| Starlight 2 Green"
     },
             delimiter = '|')
-    @Disabled
+
     @ParameterizedTest(name = "Проверка наличия в выдаче поиска по коду {0} товара" +
-            " названий бренда {1} и модели {2}")
+            " названия бренда {1} и модели {2}")
     void sportMarafonTestSearch(String searchQuery, String expectedBrand, String expectedName) {
+        open("https://sport-marafon.ru/");
         $(".header__search").click();
         $("#head-search-input").setValue(searchQuery).pressEnter();
         $(".product-list__brand").shouldHave(text(expectedBrand));
         $(".product-list__name").shouldHave(text(expectedName));
 
     }
-
-
 }
